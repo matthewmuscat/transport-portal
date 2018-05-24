8f58d1517122722214df324871d864d84ae82fd0
Leon Sandøy <leon.sandoy@conmodo.com>
2018-05-24 23:23:52 +0200

    Setting up database factories. Everything is still pretty broken, but I'll fix that tomorrow.

--- app_test.py
+++ app_test.py
@@ -22,15 +22,4 @@ class SiteTest(TestCase):
         return app
 
 
-class RootEndpoint(SiteTest):
-    """
-    Test cases for the root endpoint and error handling
-    """
 
-    def test_index(self):
-        """
-        Check the root path responds with 200 OK
-        """
-
-        response = self.client.get('/', "http://"+app.config['SERVER_NAME'])
-        self.assertEqual(response.status_code, 200)
