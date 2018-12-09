3bf9724aae230e557871b9a71b284c5423ce4d6c
Leon Sandøy <leon.sandoy@conmodo.com>
2018-12-09 22:40:47 +0100

    fix typo

--- portal/settings.py
+++ portal/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-from plugins.crispy_forms_bulma.settings import *  # noqa
+from plugins.crispy_bulma.settings import *  # noqa
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
