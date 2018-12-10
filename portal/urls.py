820c138ec894d4fb2ffa6af0d2f443e49553f810
Leon Sandøy <leon.sandoy@conmodo.com>
2018-12-10 20:10:26 +0100

    Seeing if I can serve them like this

--- portal/urls.py
+++ portal/urls.py
@@ -1,8 +1,11 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+from portal import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("forms/", include("driver_forms.urls")),
     path("", include("asset_manager.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
